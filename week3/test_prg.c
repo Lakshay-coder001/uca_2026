a4066af5d792d17344c8403c5cf2d25b2cd86489
Lakshay Kumar <lakshay.in0001@gmail.com>
2026-07-26 23:19:50 +0530

    comment remover added

---
+++ week3/test_prg.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* 
+   Test Case 1: Standard multi-line comment.
+   Your code should remove this entire block completely.
+*/
+
+// Test Case 2: Standard single-line comment. This should be removed.
+int main(void) {
+    // Test Case 3: Inline single-line comment.
+    int a = 10; /* Test Case 4: Inline multi-line comment. */
+
+    /* Test Case 5: Multi-line comment 
+       spanning over 
+       multiple lines inside a function */
+    int b = 20;
+
+
+    //
+    int c = 10;
+    // -------------------------------------------------------------
+    // EDGE CASES: The scenarios below should NOT be modified!
+    // -------------------------------------------------------------
+
+    
+}
