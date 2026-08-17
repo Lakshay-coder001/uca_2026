520b1aaa53b5dc6b56ae82dc3f88a533ef22b494
Lakshay Kumar <lakshay.in0001@gmail.com>
2026-08-17 15:06:50 +0530

    [address translation solution added]

---
+++ week6/linux/address_translation.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+void address_translation(uint32_t virtual_address) {
+    uint32_t page_number = 0;
+    uint32_t offset = 0;
+
+    // TODO: Calculate page_number using a right-shift operation (>>)
+    page_number = virtual_address >> 12;
+    
+
+    // TODO: Calculate offset using a bitwise AND operation (&) with mask 0xFFF
+    offset = virtual_address &(0xFFF);
+
+    printf("[1A] Addr: 0x%08X -> Page: %u (0x%X), Offset: %u (0x%X)\n",
+           virtual_address, page_number, page_number, offset, offset);
+}
+
+int main(){
+    uint32_t virtual_address;
+    printf("Enter address: ");
+    scanf("%x", &virtual_address );
+
+    address_translation(virtual_address);
+}
