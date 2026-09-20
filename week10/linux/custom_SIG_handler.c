08af38d97128d6dd674794fdb201e2f28afd4495
Lakshay Kumar <lakshay.in0001@gmail.com>
2026-09-20 14:44:38 +0530

    [added solution of SIG_Handler]

---
+++ week10/linux/custom_SIG_handler.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+// Global variable to count signal triggers
+volatile sig_atomic_t signal_count = 0;
+
+// TODO: Write your signal handler function here
+void handle_sigint(int sig) {
+    signal_count++;
+    printf("Ctrl+C pressed: (times)%d\n",signal_count);
+    if(signal_count == 3){
+        kill(getpid(),SIGKILL);
+    }
+}
+
+int main() {
+    // TODO: Register the signal handler.
+    signal(SIGINT , handle_sigint);
+    printf("Program running (PID: %d). Try pressing Ctrl+C...\n", getpid());
+    
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+}
